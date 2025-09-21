5da14c2f2fef4a833e91aba22e9a78790bb5bb60
Reinhard Willig <rw@reinhardwillig.de>
2025-09-21 16:00:43 +0200

    PHP Mathematische Operatoren

---
+++ Operatoren in PHP/Mathematische Operatoren/index.php
@@ -0,0 +1,29 @@
+<?php
+/*************************************************
+ * Copyright (c) Reinhard Willig
+ * Date: 21.09.2025
+ * Time: 15:50
+ *************************************************/
+
+$a = 5;
+$b = 3;
+
+// Addition
+$summe = $a + $b;
+
+// Substraktion
+$summe = $a - $b;
+
+// Multiplikation
+$summe = $a * $b;
+
+// Division
+$summe = $a / $b;
+
+// Modulo
+$summe = $a % $b;
+
+// Exponentiation
+$summe = $a ** $b;
+
+echo $summe;
