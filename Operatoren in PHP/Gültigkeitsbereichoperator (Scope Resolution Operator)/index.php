7f46ee212dad08f2c2381a7d474e60b5d5f9c0fe
Reinhard Willig <rw@reinhardwillig.de>
2025-09-22 12:56:05 +0200

    Gültigkeitsbereichoperator (Scope Resolution Operator)

--- Operatoren in PHP/Gültigkeitsbereichoperator (Scope Resolution Operator)/index.php
+++ Operatoren in PHP/Gültigkeitsbereichoperator (Scope Resolution Operator)/index.php
@@ -4,25 +4,33 @@
  * Date: 22.09.2025
  * Time: 12:24
  *************************************************/
- 
- class User{
-     const AGE = 30;
-       public static $name = 'Reinhard';
-       public static function greets(){
-          return "Hello ".self::$name;
-       }
- };
 
-class Hobby extends User{
-    public static function cooking(){
+class User
+{
+   const AGE = 30;
+   public static $name = 'Reinhard';
 
-        return parent::$name;
-    }
-};
-   echo User::greets(); // Hello Reinhard
-   echo "<br>";
-   echo User::$name;    // Reinhard
-   echo "<br>";
-   echo User::AGE."<br>";      // 30
+   public static function greets()
+   {
+      return "Hello " . self::$name;
+   }
+}
 
-    echo Hobby::cooking(); // Reinhard
+;
+
+class Hobby extends User
+{
+   public static function cooking()
+   {
+      return parent::$name;
+   }
+}
+
+;
+echo User::greets(); // Hello Reinhard
+echo "<br>";
+echo User::$name;    // Reinhard
+echo "<br>";
+echo User::AGE . "<br>";      // 30
+
+echo Hobby::cooking(); // Reinhard
