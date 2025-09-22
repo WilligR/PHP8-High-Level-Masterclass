f94b62bf553b5adbdf6b197991ab5458e2b3bb08
Reinhard Willig <rw@reinhardwillig.de>
2025-09-22 10:40:39 +0200

    Verkettungsoperator (Konkatenation)

---
+++ Operatoren in PHP/Verkettungsoperator (Konkatenation)/index.php
@@ -0,0 +1,12 @@
+<?php
+/*************************************************
+ * Copyright (c) Reinhard Willig
+ * Date: 22.09.2025
+ * Time: 10:35
+ *************************************************/
+
+$text1 = "Hallo";
+$text2 = "Welt";
+
+$text3 = $text1 . " " . $text2 . "!";
+echo $text3 . "<br>"; // Hallo Welt!
